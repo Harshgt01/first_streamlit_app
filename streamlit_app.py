0a888dde77ab2cf67bfaf3b62ba2b4457ab69a97
Harshgt01 <134089212+Harshgt01@users.noreply.github.com>
2023-06-06 16:07:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ try:
     if not fruit_choice:
                 streamlit.error("please select a fruit to get information.")
     else:
-        back_from_function= get_fruityvice_data()
+        back_from_function= get_fruityvice_data(fruit_choice)
         streamlit.dataframe(back_from_function)
     #def get_fruity_vice_data(this_fruit_choice):
      #    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
