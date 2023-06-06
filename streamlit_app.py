64172ba530aad6319a58fb475da07f852646de4a
Harshgt01 <134089212+Harshgt01@users.noreply.github.com>
2023-06-06 16:16:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,10 +38,6 @@ try:
     else:
         back_from_function= get_fruityvice_data(fruit_choice)
         streamlit.dataframe(back_from_function)
-    #def get_fruity_vice_data(this_fruit_choice):
-     #    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-      #   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-       #  return fruityvice_normalized
 except URLError as e:
  streamlit.error()
  streamlit.stop()
